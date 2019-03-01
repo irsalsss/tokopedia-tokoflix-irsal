e071c40455bd65b396a359cef3b1ce1efd1ee050
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 17:26:44 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/detail/:id" component={DetailPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
