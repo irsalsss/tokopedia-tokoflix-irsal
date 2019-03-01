107f3289b43ad451cba8882e4f867e039a83c872
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 14:06:08 +0700

    add HomePage as the main page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from '../components/NavBar';
+
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div className="container-card">
+        <NavBar />
+      </div>           
+    )
+  }
+}
+
+export default HomePage;
