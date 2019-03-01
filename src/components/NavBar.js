ce1d41d6cfd728b34fe920b2c8ee5e4127c25782
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-02 01:50:23 +0700

    add link config

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+// import component
 import tokopedia from '../assets/tokopedia.png'
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="nav-wrapper black">
-        <img style={{width: '160px', height: '40px', marginTop: '13px', marginLeft: '20px', cursor: 'pointer'}} src={tokopedia} alt="tokopedia"/>
+        <Link to='/'>
+          <img style={{width: '160px', height: '40px', marginTop: '13px', marginLeft: '20px', cursor: 'pointer'}} src={tokopedia} alt="tokopedia"/>
+        </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down"> 
-          <li className="text-white">TOKOFLIX</li>
+          <Link to='/'><li className="text-white">TOKOFLIX</li></Link>
         </ul>
       </nav>
       
