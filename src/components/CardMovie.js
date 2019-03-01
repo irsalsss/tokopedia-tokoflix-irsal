6f5be7c771c55adfcb56b9bb254dd7c7da6be769
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-02 03:18:59 +0700

    add some style

--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -5,14 +5,14 @@ class CardMovie extends Component {
     const { movie } = this.props;
     const posterImage = 'http://image.tmdb.org/t/p/w500';
     return (
-      <div className="card" style={{width: '250px', height: '550px'}}>
+      <div className="card hoverable" style={{width: '250px', height: '550px'}}>
         <div className="card-image">
           <img src={posterImage + movie.poster_path} alt={movie.title} style={{width: '250px', heigth: '550px', borderTopLeftRadius: '10px', borderTopRightRadius: '10px'}} />
         </div>
         <div className="card-content">
           <p className='style-text'>{movie.title}</p>
           <i className='bx bxs-star bx-gold top-pos'><span className="margin-left">{movie.vote_average}</span></i><br />
-          <a href="/" className="btn t-green">Rp 100.000</a>
+          <button className="btn t-green">Rp 100.000</button>
         </div>
       </div>
     )
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -4,7 +4,7 @@ import { fetchApiDetail } from '../actions/detailApiActions';
 
 class DetailPage extends Component {
   state = {
-    movies: []
+    movies: {}
   }
 
   componentDidMount() {
@@ -18,11 +18,47 @@ class DetailPage extends Component {
       })
   }
 
+  
   render() {
-    // console.log(this.state);
+    console.log(this.state);
+    // const urlImg = 'https://image.tmdb.org/t/p/original' + this.state.movies.backdrop_path;
+    // style={{backgroundColor: 'black', height: '100vp', width: '100vp'}}
+    const posterImage = 'http://image.tmdb.org/t/p/w500';
+
     return (
-      <div className="" >
-        {this.state.movies.title}  
+      <div className="container-detail-page">
+        <div className="box-movie">
+          <img className="float-left" src={posterImage + this.state.movies.poster_path} alt={this.state.movies.title} style={{height: '550px'}} />
+          <div className="movie-detail">
+            <div className="movie-title">{this.state.movies.title}</div>
+            <div className="movie-tagline">{this.state.movies.tagline}</div>
+            <div className="movie-overview">{this.state.movies.overview}</div>
+            <div className="movie-overview-bot">Cast:</div>
+            <div className="movie-tagline-bot">BAGIAN INI MASIH ERROR</div>
+            <div className="container-bot">
+              <div className="float-left-bot">
+                <div className="movie-overview-bot">Running Time</div>
+                <div className="movie-tagline-bot">{this.state.movies.runtime} mins</div>
+                <div className="movie-overview-bot">Harga</div>
+                <div className="movie-tagline-bot">Rp 100.000</div>
+              </div>
+              <div className="float-left-bot">
+                <div className="movie-overview-bot">Rating</div>
+                <div className="movie-tagline-bot">{this.state.movies.vote_average} / 10</div>
+                <div className="movie-overview-bot">Purchased</div>
+                <button className="btn red btn-purchase">Not Yet</button>
+              </div>
+              <div className="float-left-bot btn-buy">
+                <button className="btn disabled">Rp 100.000
+                  <i className="material-icons left">account_balance_wallet</i>
+                </button>
+                <button className="btn waves-effect waves-light mrgn" type="submit" name="action">BUY
+                  <i className="material-icons left">shopping_cart </i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
