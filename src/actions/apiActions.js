3bf77b76b9c48d640006c7e271a78dd9fad88a22
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 18:02:20 +0700

    edit api url

--- src/actions/apiActions.js
+++ src/actions/apiActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchApi = (endpoint) => (dispatch, getState) => {
-  return axios.get('https://api.themoviedb.org/3/discover/movie?&api_key=00d559fae317a5e2c2d956106b5ad3b0&page=1&language=en-US&sort_by=popularity')
+  return axios.get('https://api.themoviedb.org/3/discover/movie?&api_key=00d559fae317a5e2c2d956106b5ad3b0&page=1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=')
     .then((response) => dispatch ({
       type: 'FETCH_API_SUCCESS',
       endpoint,
