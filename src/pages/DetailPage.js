c9731f48ca8230d393461f3f7822b92bda7cf0c8
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 22:10:12 +0700

    add get api config

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchApiDetail } from '../actions/detailApiActions';
 
 class DetailPage extends Component {
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    let id = this.props.match.params.id
+    this.props.fetchApiDetail(id)
+      .then((res) => {
+        // console.log(res);
+        this.setState({
+          movies: res.response.data
+        })
+      })
+  }
+
   render() {
+    // console.log(this.state);
     return (
       <div className="" >
-        <p> test </p>
+        {this.state.movies.title}  
       </div>
     )
   }
 }
 
-export default DetailPage;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchApiDetail: (endpoint) => dispatch(fetchApiDetail(endpoint))
+  }
+}
+export default connect(null, mapDispatchToProps) (DetailPage);
