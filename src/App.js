9adcccf5fc093e5ef396df3593ceae9e248a4f5d
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-02 01:57:35 +0700

    add NavBar Component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Provider store={store}>
+          <NavBar />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/:id/:title" component={DetailPage} />
