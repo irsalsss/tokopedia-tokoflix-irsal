4cca5b1386525655cdd5306fd30dfd78bc9a51d6
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 17:25:40 +0700

    add new page for detail every movie

---
+++ src/pages/DetailPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DetailPage extends Component {
+  render() {
+    return (
+      <div className="" >
+        <p> test </p>
+      </div>
+    )
+  }
+}
+
+export default DetailPage;
