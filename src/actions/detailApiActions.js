71fad576e552a36b65e891b6ead7ae9c1931d4c7
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-02 00:57:09 +0700

    change get api link

--- src/actions/detailApiActions.js
+++ src/actions/detailApiActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchApiDetail = (moviesid) => (dispatch, getState) => {
   console.log(moviesid);
-  return axios.get(`https://api.themoviedb.org/3/movie/${moviesid}?api_key=00d559fae317a5e2c2d956106b5ad3b0&page=1&language=en-US`)
+  return axios.get(`https://api.themoviedb.org/3/movie/${moviesid}?api_key=00d559fae317a5e2c2d956106b5ad3b0&append_to_response=credits`)
     .then((response) => dispatch ({
       type: 'FETCH_DETAIL_API_SUCCESS',
       moviesid,
