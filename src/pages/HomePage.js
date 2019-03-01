ac1747a712021a4390991566d58abaea597faa78
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 14:26:28 +0700

    add function for get api

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import NavBar from '../components/NavBar';
 
 
 class HomePage extends Component {
 
+  state = {
+    movies: []
+  }
+
+  // get api using axios
+  componentDidMount(){
+    axios.get(`https://api.themoviedb.org/3/discover/movie?&api_key=00d559fae317a5e2c2d956106b5ad3b0&page=1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=`)
+    .then((res) => {
+      this.setState({
+        movies: res.data.results
+      })
+    })
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <div className="container-card">
         <NavBar />
