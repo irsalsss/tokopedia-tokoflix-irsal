11a2fca5a31c0ed5d425bca52b98921d91fa723c
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-01 22:09:24 +0700

    add detailApiReducer to combineRreducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 // import component / pages
 import apiReducer from './apiReducer';
+import detailApiReducer from './detailApiReducer';
 
 const rootReducer = combineReducers({
   fetchApi: apiReducer,
-});
+  fetchDetailApi: detailApiReducer
+})
 
 export default rootReducer;
