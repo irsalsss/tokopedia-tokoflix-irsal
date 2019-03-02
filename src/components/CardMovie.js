36f72c2be93201b35afecaa6f21b4a0d8f6e1a8a
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-02 15:19:16 +0700

    add method check price

--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
+//import component
+import method from '../utils/method';
+
 class CardMovie extends Component {
+  state = {
+    price: ''
+  }
+
   render() {
     const { movie } = this.props;
     const posterImage = 'http://image.tmdb.org/t/p/w500';
+    this.price = method.checkPrice(movie.vote_average);
     return (
       <div className="card hoverable" style={{width: '250px', height: '550px'}}>
         <div className="card-image">
@@ -12,7 +20,7 @@ class CardMovie extends Component {
         <div className="card-content">
           <p className='style-text'>{movie.title}</p>
           <i className='bx bxs-star bx-gold top-pos'><span className="margin-left">{movie.vote_average}</span></i><br />
-          <button className="btn t-green">Rp 100.000</button>
+          <button className="btn t-green">Rp {this.price}</button>
         </div>
       </div>
     )
