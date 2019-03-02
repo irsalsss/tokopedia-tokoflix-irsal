0426b2d080aba3ae45a21537771089843092dce8
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-03 01:54:17 +0700

    add recApiReducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 // import component / pages
 import apiReducer from './apiReducer';
 import detailApiReducer from './detailApiReducer';
+import recApiReducer from './recApiReducer';
 
 const rootReducer = combineReducers({
   fetchApi: apiReducer,
-  fetchDetailApi: detailApiReducer
+  fetchDetailApi: detailApiReducer,
+  fetchRecApi: recApiReducer
 })
 
 export default rootReducer;
